229fac67188e07770791b2c29612172bc79d1117
carlitaaaaaaaaaaaaaaaaa <martugubi2003@gmail.com>
2019-05-22 09:19:27 -0300

    Update ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,5 @@
+var secretnumber=8
+var 8=prompt("elegi un numero")
+if(secretnumber===8){console.log("correct")}else{"incorrect"}
+if(secretnumber<8) console.log("incorrect")
+if(secretnumber>8) console.log("incorrect")
